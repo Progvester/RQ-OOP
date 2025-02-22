7019af05b68400e7c6caa4bec9510339a3c2e740
stamalex <stamalex2000@gmail.com>
2025-02-22 18:05:55 +0200

    get random quotes via public api service

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -8,6 +8,13 @@ class RandomQuote {
     const { id, text, author } = quotes[randomIndex];
     return new Quote(id, text, author);
   }
+  static getRandomQuoteViaAPI() {
+    const url = 'https://quoteslate.vercel.app/api/quotes/random';
+    return fetch(url, { headers: { 'Content-Type': 'application/json' } })
+      .then((response) => response.json())
+      .then(({ id, quote, author }) => new Quote(id, quote, author))
+      .catch((error) => console.warn(error));
+  }
 }
 
 export default RandomQuote;
